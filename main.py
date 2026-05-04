ba0c04ddaa4560a9504c8d5de61e96b8dc07bd53
Morshed Alam <ma.morshedalampro@gmail.com>
2026-05-04 18:52:37 +0600

    Wrap Huck Finn text and add NumPy loader
    Reformatted data/huck_finn.txt by breaking the single long line into multiple lines for readability. In main.py, commented out the previous manual read/write/context-manager examples and added a NumPy-based loader: import numpy as np and use np.loadtxt(filename, delimiter=',', skiprows=1, usecols=[0, 1]) to read two columns from a CSV-like text file and print the result. Comments clarify the delimiter, header skip, and selected columns.

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 """ READING A TEXT FILE """
-filename = 'data/huck_finn.txt'
-file = open(filename, mode='r') # 'r' stands for 'read'
-text = file.read()
-file.close() # Always close the file after you're done with it
+# filename = 'data/huck_finn.txt'
+# file = open(filename, mode='r') # 'r' stands for 'read'
+# text = file.read()
+# file.close() # Always close the file after you're done with it
 
 """ WRITING TO A FILE """
-filename = 'data/huck_finn.txt'
-file = open(filename, mode='w') # 'w' stands for 'write'
-file.write("Hello, World!")
-file.close() # Always close the file after you're done with it
+# filename = 'data/huck_finn.txt'
+# file = open(filename, mode='w') # 'w' stands for 'write'
+# file.write("Hello, World!")
+# file.close() # Always close the file after you're done with it
 
 """ CONTEXT MANAGER WITH """
-with open('data/huck_finn.txt', 'r') as file:
-    text = file.read()
-    print(text)
+# with open('data/huck_finn.txt', 'r') as file:
+#     text = file.read()
+#     print(text)
+
+""" NumPy IMPORT """
+import numpy as np
+
+filename = 'data/huck_finn.txt'
+data = np.loadtxt(filename, delimiter=',', skiprows=1, usecols=[0, 1]) 
+# Load data from a text file, skipping the first row (header)
+# default delimiter is whitespace, but here we specify a comma for CSV files
+# usecols specifies which columns to read (0 and 1 in this case)
+print(data)
