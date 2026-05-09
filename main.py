e0de6c8381ef50601a8c216c0a168a47283702f6
Morshed Alam <ma.morshedalampro@gmail.com>
2026-05-09 11:42:40 +0600

    Convert IO examples to commented snippets
    Replace the active NumPy file load/print with a set of commented example blocks demonstrating various file I/O patterns. main.py now contains commented snippets for NumPy, Pandas (CSV/Excel), pickle, SAS, Stata, and HDF5 usage, and cleaned/standardized docstring headings. The previous active np.loadtxt usage and print were commented out to turn the file into a reference of sample import/read patterns without executing any I/O.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-""" READING A TEXT FILE """
+"""READING A TEXT FILE"""
 # filename = 'data/huck_finn.txt'
 # file = open(filename, mode='r') # 'r' stands for 'read'
 # text = file.read()
 # file.close() # Always close the file after you're done with it
+
 
 """ WRITING TO A FILE """
 # filename = 'data/huck_finn.txt'
@@ -10,17 +11,66 @@
 # file.write("Hello, World!")
 # file.close() # Always close the file after you're done with it
 
+
 """ CONTEXT MANAGER WITH """
 # with open('data/huck_finn.txt', 'r') as file:
 #     text = file.read()
 #     print(text)
 
-""" NumPy IMPORT """
-import numpy as np
 
-filename = 'data/huck_finn.txt'
-data = np.loadtxt(filename, delimiter=',', skiprows=1, usecols=[0, 1]) 
-# Load data from a text file, skipping the first row (header)
-# default delimiter is whitespace, but here we specify a comma for CSV files
-# usecols specifies which columns to read (0 and 1 in this case)
-print(data)
+""" NumPy IMPORT """
+# import numpy as np
+# filename = 'data/huck_finn.txt'
+# data = np.loadtxt(filename, delimiter=',', skiprows=1, usecols=[0, 1])
+# # Load data from a text file, skipping the first row (header)
+# # default delimiter is whitespace, but here we specify a comma for CSV files
+# # usecols specifies which columns to read (0 and 1 in this case)
+# print(data)
+
+
+""" PANDAS IMPORT """
+# import pandas as pd
+# filename = 'data/huck_finn.txt'
+# data = pd.read_csv(filename, delimiter=',') # Read a CSV file into a DataFrame
+# data = pd.read_csv(filename, nrows=10, header=None) # nrows specifies how many rows to read, header=None indicates that the file does not have a header row
+# print(data.head()) # Print the first few rows of the DataFrame
+
+# data_array = data.to_numpy() # Convert the DataFrame to a NumPy array
+# print(data_array)
+
+
+""" PICKLED FILES """
+# import pickle
+# with open("pickled_data.pkl", "rb") as file:  # 'rb' stands for 'read binary'
+#     data = pickle.load(file)  # Load the pickled data
+# print(data)
+
+
+""" Excel spreadsheet """
+# import pandas as pd
+# file = 'urbanpop.xlsx'
+# data = pd.ExcelFile(file)  # Load the Excel file
+# print(data.sheet_names)  # Print the names of the sheets in the Excel file
+
+# df1 = data.parse('1960-1965')  # Parse the sheet named '1960-1965' into a DataFrame
+# df2 = data.parse(0) # Parse the first sheet (index 0) into a DataFrame
+
+
+""" IMPORTING SAS FILES """
+# import pandas as pd
+# from sas7BDAT import SAS7BDAT
+# with SAS7BDAT('urbanpop.sas7bdat') as file:
+#     df_sas = file.to_data_frame() # Convert the SAS file to a DataFrame
+
+
+""" IMPORTING STATA FILES """
+# import pandas as pd
+# data = pd.read_stata('urbanpop.dta') # Read a Stata file into a DataFrame
+# print(data.head()) # Print the first few rows of the DataFrame
+
+
+""" IMPORTING HDF5 FILES """
+# import h5py
+# filename = 'data/urbanpop.hdf5'
+# data = h5py.File(filename, 'r') # 'r' stands for 'read'
+# print(data.keys()) # Print the keys (datasets) in the HDF5 file
